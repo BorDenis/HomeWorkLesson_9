7b829a1355434254ceefd88a31d2a8c45e3b2b98
Denis Borodin <borodin.denis.23@gmail.com>
2022-03-16 23:43:07 +0300

    added new class

---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,9 @@
+package tests;
+
+import java.util.Arrays;
+
+public class TestData {
+
+    public Object[][] testData = new Object[][]{{1, 2}, {null, null}, {"three", "four"}, {true, false}};
+
+}
